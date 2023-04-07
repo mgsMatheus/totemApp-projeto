af98d556864ef04a92771a17519754407d6327f9
Matheus Gomes <7kiks7@gmail.com>
2023-04-07 15:15:20 -0300

    Ponto de Partida

---
+++ src/main/java/br/com/fiapchallenge/totemApp/TotemAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiapchallenge.totemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotemAppApplication.class, args);
+	} 
+
+}
---
+++ src/test/java/br/com/fiapchallenge/totemApp/TotemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiapchallenge.totemApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
